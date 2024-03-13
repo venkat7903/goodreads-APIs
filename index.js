0792e0d3450fef06900247e2b5f44a6776e9e90e
venkat <raja.srigakollu1234@gmail.com>
2024-03-13 16:43:28 +0000

    com

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
 const express = require("express");
 const path = require("path");
-
+const cors = require("cors");
 const { open } = require("sqlite");
 const sqlite3 = require("sqlite3");
 
 const app = express();
 app.use(express.json());
+
+app.use(
+  cors({
+    origin: "*",
+  })
+);
+
+app.use(
+  cors({
+    methods: ["GET", "POST", "DELETE", "UPDATE", "PUT", "PATCH"],
+  })
+);
 
 const dbPath = path.join(__dirname, "goodreads.db");
 
